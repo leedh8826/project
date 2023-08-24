2f337a76838c6678616c1f78f40a3edf381d50bd
leedh8826 <leedh8826@gmail.com>
2023-08-24 08:13:45 +0000

    화면 추기 및 server.js 수정

--- harmful_web/server.js
+++ harmful_web/server.js
@@ -1,29 +1,19 @@
-// server.js
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const port = 3000; // 사용할 포트 번호를 지정합니다
+const port = 3000;
 
 app.use((req, res, next) => {
-  // 3초 지연 후 다음 미들웨어로 이동
-  setTimeout(next, 3000);
+    // 3초 지연 후 다음 미들웨어로 이동
+    setTimeout(next, 3000);
 });
 
-app.use(cors()); // CORS 설정
-
-app.post('/api/add-domain', (req, res) => {
-  // 추가 로직을 구현하세요
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+    next();
 });
 
-app.get('/api/get-harmful-domains', (req, res) => {
-  // 데이터를 가져오는 로직을 구현하세요
-});
-
-app.post('/api/delete-domains', (req, res) => {
-  // 삭제 로직을 구현하세요
-});
+app.use(express.static('pages'));
 
 app.listen(port, () => {
-  console.log(`Express server is running on port ${port}`);
+    console.log(`서버가 포트 ${port}에서 실행 중입니다.`);
 });
